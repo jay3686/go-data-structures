2ce9dd5e7921a8dc7ea3132a64441f81cd048039
Jay Patel <jay3686@gmail.com>
2017-05-28 20:27:22 -0400

    add a couple multi insertion tests

--- heap/maxheap_test.go
+++ heap/maxheap_test.go
@@ -2,10 +2,38 @@ package heap
 
 import "testing"
 
-func TestInsert(t *testing.T) {
+import "reflect"
+
+func TestInsertOne(t *testing.T) {
 	m := MaxHeap{}
 	m.Insert(1)
 	if m.data[0] != 1 {
 		t.Error("Single element insert fail")
+	}
+}
+
+func TestInsertManyDesc(t *testing.T) {
+	m := MaxHeap{}
+	nums := []int{6, 5, 4, 3, 2, 1}
+
+	for _, n := range nums {
+		m.Insert(n)
+	}
+
+	if !reflect.DeepEqual(m.data, []int{6, 5, 4, 3, 2, 1}) {
+		t.Error("Multiple element insert fail")
+	}
+}
+
+func TestInsertManyAsc(t *testing.T) {
+	m := MaxHeap{}
+	nums := []int{1, 2, 3, 4, 5, 6}
+
+	for _, n := range nums {
+		m.Insert(n)
+	}
+
+	if !reflect.DeepEqual(m.data, []int{6, 5, 4, 3, 2, 1}) {
+		t.Error("Multiple element insert fail")
 	}
 }
