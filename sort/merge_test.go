b8d14dc96bb61c4e25ba8ae1a102322dcd04ef99
Jay Patel <jay3686@gmail.com>
2017-06-07 00:02:30 -0400

    merge sort test and benchmark

---
+++ sort/merge_test.go
@@ -0,0 +1,24 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	nums := []int{6, 5, 4, 3, 2, 1}
+	MergeSort(nums)
+	if !reflect.DeepEqual(nums, []int{1, 2, 3, 4, 5, 6}) {
+		t.Error("Not sorted in asc order", nums)
+	}
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	nums := []int{}
+	for i := 1000; i >= -1000; i-- {
+		nums = append(nums, i)
+	}
+	for i := 0; i < b.N; i++ {
+		MergeSort(nums)
+	}
+}
