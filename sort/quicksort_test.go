bdc06053c0b9bf0d8602ee28f9fc6e3b288103d0
Jay Patel <jay3686@gmail.com>
2017-06-08 21:24:23 -0400

    benchmark and test

---
+++ sort/quicksort_test.go
@@ -0,0 +1,24 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	nums := []int{6, 5, 4, 3, 2, 1}
+	QuickSort(nums)
+	if !reflect.DeepEqual(nums, []int{1, 2, 3, 4, 5, 6}) {
+		t.Error("Not sorted in asc order", nums)
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	nums := []int{}
+	for i := 1000; i >= -1000; i-- {
+		nums = append(nums, i)
+	}
+	for i := 0; i < b.N; i++ {
+		QuickSort(nums)
+	}
+}
